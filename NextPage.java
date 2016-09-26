79504f7b47976a90e55a7d713c713e1ac19f2a0b
Usama <usamadesai@gmail.com>
2016-09-26 16:09:33 +0530

    Add files via upload

---
+++ NextPage.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+  
+class NextPage extends JFrame
+ {
+   NextPage()
+  {
+  setDefaultCloseOperation(javax.swing.
+   WindowConstants.DISPOSE_ON_CLOSE);
+  setTitle("Welcome");
+  setSize(400, 200);
+   }
+  }
+ 
